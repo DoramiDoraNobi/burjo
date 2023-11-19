f451532c1ab8304d9a3cc623667fe7e8eeaec702
DoramiDoraNobi <mikobantul@gmail.com>
2023-11-19 20:16:41 +0700

    Database

--- burjo.sql
+++ burjo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2023 at 09:15 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Nov 19, 2023 at 02:15 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,7 +33,22 @@ CREATE TABLE `berlangganan` (
   `tanggal_mulai` date DEFAULT NULL,
   `tanggal_berakhir` date DEFAULT NULL,
   `harga` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hutang`
+--
+
+CREATE TABLE `hutang` (
+  `id_hutang` int(11) NOT NULL,
+  `id_pelanggan` int(11) DEFAULT NULL,
+  `id_pemilik` int(11) DEFAULT NULL,
+  `jumlah_hutang` decimal(10,2) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `status_pembayaran` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -48,7 +63,15 @@ CREATE TABLE `pelanggan` (
   `nomor_telepon` varchar(20) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
   `catatan_tambahan` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `id_pemilik`, `nama_pelanggan`, `nomor_telepon`, `alamat`, `catatan_tambahan`) VALUES
+(2, 1, 'Pak Embul', '4223335', 'Embun Senja Jakarta', 'Sudah Lunas kemarin'),
+(6, 2, 'Dimas Bayu', '3332423', 'Klaten', 'Belum bayar 5 tahun');
 
 -- --------------------------------------------------------
 
@@ -62,7 +85,7 @@ CREATE TABLE `pemilikwarmindo` (
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `tanggal_daftar` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pemilikwarmindo`
@@ -81,6 +104,14 @@ INSERT INTO `pemilikwarmindo` (`id_pemilik`, `nama_warung`, `email`, `password`,
 --
 ALTER TABLE `berlangganan`
   ADD PRIMARY KEY (`id_berlangganan`),
+  ADD KEY `id_pemilik` (`id_pemilik`);
+
+--
+-- Indexes for table `hutang`
+--
+ALTER TABLE `hutang`
+  ADD PRIMARY KEY (`id_hutang`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
   ADD KEY `id_pemilik` (`id_pemilik`);
 
 --
@@ -107,10 +138,16 @@ ALTER TABLE `berlangganan`
   MODIFY `id_berlangganan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `hutang`
+--
+ALTER TABLE `hutang`
+  MODIFY `id_hutang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `pemilikwarmindo`
@@ -127,6 +164,13 @@ ALTER TABLE `pemilikwarmindo`
 --
 ALTER TABLE `berlangganan`
   ADD CONSTRAINT `berlangganan_ibfk_1` FOREIGN KEY (`id_pemilik`) REFERENCES `pemilikwarmindo` (`id_pemilik`);
+
+--
+-- Constraints for table `hutang`
+--
+ALTER TABLE `hutang`
+  ADD CONSTRAINT `hutang_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`),
+  ADD CONSTRAINT `hutang_ibfk_2` FOREIGN KEY (`id_pemilik`) REFERENCES `pemilikwarmindo` (`id_pemilik`);
 
 --
 -- Constraints for table `pelanggan`
